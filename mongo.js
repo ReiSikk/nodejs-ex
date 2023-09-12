817855043868bc8569ca744f0390a1e88a80b0e8
Rei Sikk <reix0010@stud.kea.dk>
2023-09-12 16:24:56 +0200

    POSTing to MongoDb working but with an console error..

--- mongo.js
+++ mongo.js
@@ -3,40 +3,84 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 const hostname = "127.0.0.1";
 const port = 4000;
 
-let uri = 'mongodb://localhost:27017'
-
-const server = http.createServer((req, res) => { 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Node server stuff");
-});
-
-server.listen(port, hostname, () => {
-    console.log(`server running at http://${hostname}:${port}/`)
-});
-
+let uri = 'mongodb://127.0.0.1:27017/'
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri,  {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+  }
 }
 );
 
-async function run() {
+
+async function insert(name, shape, style) {
+
+  const myDB = client.db("myDB");
+  const myColl = myDB.collection("pizzaMenu");
+  
+  const doc = { 
+    name: name,
+    shape: shape,
+    style: style
+  };
+  const result = await myColl.insertOne(doc);
+console.log(
+`A document was inserted with the _id: ${result.insertedId}`,
+);
+return result.insertedId;
+}
+
+
+
+async function run(data) {
     try {
       // Connect the client to the server (optional starting in v4.7)
       await client.connect();
+
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+      await insert(data.name, data.shape, data.style);
+
+    } catch (error) {
+      //handle the error
+      console.error(error);
     } finally {
       // Ensures that the client will close when you finish/error
       await client.close();
     }
   }
 
-  run().catch(console.dir);
+  const server = http.createServer((req, res) => { 
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+
+    if(req.method === 'POST') {
+      let body = '';
+
+      req.on('data', chunk => {
+        body += chunk.toString();
+      });
+
+      req.on('end',  () => {
+        const parsedBody = JSON.parse(body);
+         run(parsedBody).then(function(id) {
+          res.writeHead(201, { "Content-Type": "application/json" });
+          res.end(JSON.stringify({ pizza: id}));
+        });
+      });
+    } 
+      res.end("Node server stuff");
+  });
+
+server.listen(port, hostname, () => {
+    console.log(`server running at http://${hostname}:${port}/`)
+});
+
+run().catch(console.dir);
+
